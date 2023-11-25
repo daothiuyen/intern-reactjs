d58453edf1c0e259f9f93c4b078821309f0caa51
DESKTOP-9GRKLB4\Dao Uyen <uyendaothi@bizfly.vn>
2023-11-25 21:33:59 +0700

    modal add new user

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import { Container } from 'react-bootstrap';
 import './App.scss';
 import Header from './component/Header';
 import TableUsers from './component/TableUsers';
+import ModalAddNew from './component/ModalAddNew';
+import { useState } from 'react';
 
 function App() {
+
+  const [isShowModalAddNew, setIsShowModalAddNew] = useState(false);
+  const handleClose = () => {
+    setIsShowModalAddNew(false);
+  }
   return (
     <div className='app-container'>
       <Header />
       <Container >
+        <div className='my-3 add-new'>
+          <span><b>List Users:</b></span>
+          <button className='btn btn-success' onClick={() => setIsShowModalAddNew(true)}>Add new</button>
+        </div>
         <TableUsers />
       </Container>
+      <ModalAddNew
+        show={isShowModalAddNew}
+        handleClose={handleClose}
+      />
     </div>
   );
 }

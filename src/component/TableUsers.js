e6fdc6f7d658623d558fbebd46297f191607163b
DESKTOP-9GRKLB4\Dao Uyen <uyendaothi@bizfly.vn>
2023-11-25 20:51:12 +0700

    paginate

--- src/component/TableUsers.js
+++ src/component/TableUsers.js
@@ -1,21 +1,32 @@
 import { useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import { fetchAllUser } from '../services/UserService';
+import ReactPaginate from 'react-paginate';
 
 const TableUsers = () => {
 
     const [listUsers, setListUser] = useState([]);
+    const [totalUsers, setTotalUsers] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+
     useEffect(() => {
-        getUsers();
+        getUsers(2);
     }, [])
 
 
-    const getUsers = async () => {
-        let res = await fetchAllUser();
+    const getUsers = async (page) => {
+        let res = await fetchAllUser(page);
         if (res && res.data) {
+            setTotalUsers(res.total);
             setListUser(res.data);
+            setTotalPages(res.total_pages);
         }
 
+    }
+
+    const handlePageClick = (event) => {
+        //dấu + ở đầu để convert string -> number
+        getUsers(+event.selected + 1);
     }
     return (
         <>
@@ -44,6 +55,25 @@ const TableUsers = () => {
 
                 </tbody>
             </Table>
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                pageCount={totalPages}
+                previousLabel="< previous"
+                renderOnZeroPageCount={null}
+                pageClassName='page-item'
+                pageLinkClassName='page-link'
+                previousClassName='page-item'
+                previousLinkClassName='page-link'
+                nextClassName='page-item'
+                nextLinkClassName='page-link'
+                breakClassName='page-item'
+                breakLinkClassName='page-link'
+                containerClassName='pagination'
+                activeClassName='active'
+            />
         </>
     )
 }
